02e025782eb0c29897921006abab878bb61dd317
Timothy Alcaide <timothy.alcaide@gmzail.com>
2020-05-22 15:12:32 +0200

    add reducers

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,19 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
-// components
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
 import * as fromComponents from './components';
-
-// containers
 import * as fromContainers from './containers';
-
-// services
 import * as fromServices from './services';
+import { reducers } from './store';
 
-// routes
+
 export const ROUTES: Routes = [
   {
     path: '',
@@ -35,6 +31,7 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
+    StoreModule.forFeature('products', reducers),
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
