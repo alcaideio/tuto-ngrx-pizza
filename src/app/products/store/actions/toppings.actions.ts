b909857a8e42a4c0656f496c646e525ed330c78c
Timothy Alcaide <timothy.alcaide@gmzail.com>
2020-05-26 14:07:57 +0200

    add toppings reducers

--- src/app/products/store/actions/toppings.actions.ts
+++ src/app/products/store/actions/toppings.actions.ts
@@ -1,16 +1,23 @@
-import { createAction, props } from '@ngrx/store';
+
+import { Action } from '@ngrx/store';
 import { Topping } from '../../models/topping.model';
 
-export const loadToppings = createAction(
-    '[Product] Load Toppings',
-);
+export const LOAD_TOPPINGS = '[Products] Load Toppings';
+export const LOAD_TOPPINGS_FAIL = '[Products] Load Toppings Fail';
+export const LOAD_TOPPINGS_SUCCESS = '[Products] Load Toppings Success';
 
-export const loadToppingsSuccess = createAction(
-    '[Product] Load Toppings Success',
-    props<{ payload: Topping[] }>()
-);
+export class LoadToppings implements Action {
+  readonly type = LOAD_TOPPINGS;
+}
 
-export const loadToppingsFail = createAction(
-    '[Product] Load Toppings Fail',
-    props<{ payload: any }>()
-);
+export class LoadToppingsFail implements Action {
+  readonly type = LOAD_TOPPINGS_FAIL;
+  constructor(public payload: any) {}
+}
+
+export class LoadToppingsSuccess implements Action {
+  readonly type = LOAD_TOPPINGS_SUCCESS;
+  constructor(public payload: Topping[]) {}
+}
+
+export type ToppingsAction = LoadToppings | LoadToppingsFail | LoadToppingsSuccess;
--- src/app/products/store/reducers/index.ts
+++ src/app/products/store/reducers/index.ts
@@ -1,14 +1,31 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 import * as fromPizzas from './pizzas.reducer';
+import * as fromToppings from './toppings.reducers';
 
 export interface ProductsState {
   pizzas: fromPizzas.PizzaState;
+  toppings: fromToppings.ToppingsState;
 }
 
 export const reducers: ActionReducerMap<ProductsState> = {
   pizzas: fromPizzas.reducer,
+  toppings: fromToppings.reducer,
 };
 
-export const getProductsState = createFeatureSelector<ProductsState>(
-  'products'
-);
+export const getProductsState = createFeatureSelector<ProductsState>('products');
+
+////// utils
+
+export function createEntities<S, T>(state: S, list: Array<T>) {
+    return list.reduce(
+        (entities: { [id: number]: T }, item: any) => {
+            return {
+                ...entities,
+                [item.id]: item,
+            }
+        },
+        {
+            ...state.entities,
+        }
+    );
+}
