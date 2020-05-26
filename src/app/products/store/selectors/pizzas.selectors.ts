130fbea08d44f3f10d54d0df2ee6c6277fca3f36
Timothy Alcaide <timothy.alcaide@gmzail.com>
2020-05-26 14:59:51 +0200

    add topping selectors

--- src/app/products/store/selectors/pizzas.selectors.ts
+++ src/app/products/store/selectors/pizzas.selectors.ts
@@ -3,34 +3,46 @@ import * as fromRoot from '../../../store';
 import { Pizza } from '../../models/pizza.model';
 import * as fromFeature from '../reducers';
 import * as fromPizzas from '../reducers/pizzas.reducer';
+import * as fromToppings from '../reducers/toppings.reducers';
 
 export const getPizzaState = createSelector(
-    fromFeature.getProductsState,
-    (state: fromFeature.ProductsState) => state.pizzas
-  );
-  
-  export const getPizzasEntities = createSelector(
-    getPizzaState,
-    fromPizzas.getPizzasEntities
-  );
+  fromFeature.getProductsState,
+  (state: fromFeature.ProductsState) => state.pizzas
+);
 
-  export const getSelectedPizza = createSelector(
-    getPizzasEntities,
-    fromRoot.getRouterState,
-    (entities, router): Pizza => {
-      return router.state && entities[router.state.params.pizzaId];
-    }
-  );
-  
-  export const getAllPizzas = createSelector(getPizzasEntities, entities => {
-    return Object.keys(entities).map(id => entities[+id]);
-  });
-  
-  export const getPizzasLoaded = createSelector(
-    getPizzaState,
-    fromPizzas.getPizzasLoaded
-  );
-  export const getPizzasLoading = createSelector(
-    getPizzaState,
-    fromPizzas.getPizzasLoading
-  );
+export const getPizzasEntities = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasEntities
+);
+
+export const getSelectedPizza = createSelector(
+  getPizzasEntities,
+  fromRoot.getRouterState,
+  (entities, router): Pizza => {
+    return router.state && entities[router.state.params.pizzaId];
+  }
+);
+
+export const getPizzaVisualized = createSelector (
+  getSelectedPizza,
+  fromToppings.getToppingEntities,
+  fromToppings.getSelectedToppings,
+  (pizza, toppingsEntities, selectedToppings) => {
+    const toppings = selectedToppings.map(id => toppingsEntities[+id])
+    return { ...pizza, toppings }
+  }
+)
+
+export const getAllPizzas = createSelector(getPizzasEntities, entities => {
+  return Object.keys(entities).map(id => entities[+id]);
+});
+
+export const getPizzasLoaded = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoaded
+);
+
+export const getPizzasLoading = createSelector(
+  getPizzaState,
+  fromPizzas.getPizzasLoading
+);
---
+++ src/app/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,35 @@
+import { createSelector } from '@ngrx/store'
+import * as fromFeature from '../reducers'
+import * as fromToppings from '../reducers/toppings.reducers'
+
+export const getToppingsState = createSelector(
+    fromFeature.getProductsState,
+    (state: fromFeature.ProductsState) => state.toppings
+)
+
+export const getToppingsEntities = createSelector(
+    getToppingsState,
+    fromToppings.getToppingEntities
+)
+
+export const getSelectedToppings = createSelector(
+    getToppingsState,
+    fromToppings.getSelectedToppings
+);
+
+export const getAllToppings = createSelector(
+    getToppingsEntities,
+    entities => {
+        return Object.keys(entities).map((id => entities[+id]))
+    }
+)
+
+export const getToppingsLoaded = createSelector(
+    getToppingsState,
+    fromToppings.getToppingsLoaded
+)
+
+export const getToppingsLoading = createSelector(
+    getToppingsState,
+    fromToppings.getToppingsLoading
+)
