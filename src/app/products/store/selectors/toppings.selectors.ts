c1c1168539f9642eeb652d833b0cb60dc0a227c7
Timothy Alcaide <timothy.alcaide@gmzail.com>
2020-05-28 10:05:34 +0200

    fix visualise toppings

--- src/app/products/store/selectors/toppings.selectors.ts
+++ src/app/products/store/selectors/toppings.selectors.ts
@@ -1,13 +1,13 @@
-import { createSelector } from '@ngrx/store'
-import * as fromFeature from '../reducers'
-import * as fromToppings from '../reducers/toppings.reducers'
+import { createSelector } from '@ngrx/store';
+import * as fromFeature from '../reducers';
+import * as fromToppings from '../reducers/toppings.reducers';
 
 export const getToppingsState = createSelector(
     fromFeature.getProductsState,
     (state: fromFeature.ProductsState) => state.toppings
 )
 
-export const getToppingsEntities = createSelector(
+export const getToppingEntities = createSelector(
     getToppingsState,
     fromToppings.getToppingEntities
 )
@@ -15,10 +15,10 @@ export const getToppingsEntities = createSelector(
 export const getSelectedToppings = createSelector(
     getToppingsState,
     fromToppings.getSelectedToppings
-);
+)
 
 export const getAllToppings = createSelector(
-    getToppingsEntities,
+    getToppingEntities,
     entities => {
         return Object.keys(entities).map((id => entities[+id]))
     }
