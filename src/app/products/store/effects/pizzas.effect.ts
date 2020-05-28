dfaabf00258da7e2586a6c5597db657a5980bccc
Timothy Alcaide <timothy.alcaide@gmail.com>
2020-05-28 10:53:40 +0200

    add update pizza

--- src/app/products/store/effects/pizzas.effect.ts
+++ src/app/products/store/effects/pizzas.effect.ts
@@ -40,4 +40,18 @@ export class PizzasEffects {
     })
   );
 
+  @Effect()
+  updatePizza$ = this.actions$.pipe(
+    ofType(pizzaActions.UPDATE_PIZZA),
+    map((action: pizzaActions.UpdatePizza) => action.payload),
+    switchMap(pizza => {
+      return this.pizzaService
+        .updatePizza(pizza)
+        .pipe(
+          map(pizza => new pizzaActions.UpdatePizzaSuccess(pizza)),
+          catchError(error => of(new pizzaActions.UpdatePizzaFail(error)))
+        )
+    })
+  );
+
 }
