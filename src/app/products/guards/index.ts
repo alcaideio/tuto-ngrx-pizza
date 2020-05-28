af0642c594ecc09e348e567554f218bb3b837ca5
Timothy Alcaide <timothy.alcaide@gmail.com>
2020-05-28 15:51:27 +0200

    add pizza exists guard

--- src/app/products/guards/index.ts
+++ src/app/products/guards/index.ts
@@ -1,7 +1,9 @@
 
+import { PizzaExistsGuards } from './pizza-exists.guard';
 import { PizzasGuard } from './pizzas.guards';
 
-export const guards: any[] = [PizzasGuard];
+export const guards: any[] = [PizzasGuard, PizzaExistsGuards];
 
+export * from './pizza-exists.guard';
 export * from './pizzas.guards';
 
